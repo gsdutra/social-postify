7f430766604ea91e7f7f940295cc616bfa456446
gsdutra <gabrielsprecherdutra@gmail.com>
2023-07-15 17:25:28 -0300

    feat: add create user route

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import {IsString, IsEmail, IsNotEmpty} from 'class-validator'
+
+export class CreateUserDto {
+	@IsNotEmpty()
+	@IsString()
+	name: string;
+
+	@IsNotEmpty()
+	@IsEmail()
+	email: string;
+  
+	@IsNotEmpty()
+	@IsString()
+	password: string;
+
+	@IsNotEmpty()
+	@IsString()
+	avatar: string;
+}
---
+++ src/user/user.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UserRepository {
+
+  constructor(private prisma: PrismaService) { }
+
+  async create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async findAll() {
+    return;
+  }
+
+  async findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  async remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
