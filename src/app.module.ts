d3ad2326e093b83a7f8343e06f9352d3222d6949
gsdutra <gabrielsprecherdutra@gmail.com>
2023-07-15 16:12:44 -0300

    chore: create routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PublicationModule } from './publication/publication.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PublicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
